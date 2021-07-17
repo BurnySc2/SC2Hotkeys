78a39c61e86ce8c2ebc5469297a071507a17bb57
BurnySc2 <gamingburny@gmail.com>
2021-07-17 17:53:37 +0200

    Add hotfix for moved columns hotkeys

--- copy_hotkeys.py
+++ copy_hotkeys.py
@@ -9,6 +9,7 @@ import getpass
 if platform.system() == "Linux":
     source_hotkeys = "/home/burny/Dropbox/github/SC2Hotkeys/Burny.SC2Hotkeys"
     source_hotkeys2 = "/home/burny/Dropbox/github/SC2Hotkeys/BurnySteal.SC2Hotkeys"
+    source_hotkeys3 = "/home/burny/Dropbox/github/SC2Hotkeys/BurnyStealColumnMove.SC2Hotkeys"
 # Windows
 else:
     source_hotkeys = r"D:\Dropbox\github\SC2Hotkeys\Burny.SC2Hotkeys"
@@ -17,8 +18,10 @@ else:
 # Linux
 if platform.system() == "Linux":
     user_name = getpass.getuser()
-    normal_folder = f"/home/{user_name}/Games/battlenet/drive_c/users/{user_name}/My Documents/StarCraft II/Hotkeys"
-    accounts_folder = f"/home/{user_name}/Games/battlenet/drive_c/users/{user_name}/My Documents/StarCraft II/Accounts"
+    # normal_folder = f"/home/{user_name}/Games/battlenet/drive_c/users/{user_name}/My Documents/StarCraft II/Hotkeys"
+    # accounts_folder = f"/home/{user_name}/Games/battlenet/drive_c/users/{user_name}/My Documents/StarCraft II/Accounts"
+    normal_folder = f"/media/ssd480/Games/SC2/drive_c/users/{user_name}/My Documents/StarCraft II/Hotkeys"
+    accounts_folder = f"/media/ssd480/Games/SC2/drive_c/users/{user_name}/My Documents/StarCraft II/Accounts"
 # Windows
 else:
     user_name = os.environ["USERNAME"]
@@ -35,6 +38,9 @@ if os.path.isfile(source_hotkeys):
             if os.path.isfile(source_hotkeys2):
                 print(f"Copying {source_hotkeys2} to {target_folder}")
                 shutil.copy(source_hotkeys2, target_folder)
+            if os.path.isfile(source_hotkeys3):
+                print(f"Copying {source_hotkeys3} to {target_folder}")
+                shutil.copy(source_hotkeys3, target_folder)
 
     print(f"Copying {source_hotkeys} to {normal_folder}")
     shutil.copy(source_hotkeys, normal_folder)
