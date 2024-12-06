783590b818d508f4e79c3fe6983c33d15875dbf4
burnysc2 <gamingburny@gmail.com>
2024-12-06 21:22:31 +0100

    Update for patch 5.0.14

--- copy_hotkeys.py
+++ copy_hotkeys.py
@@ -8,7 +8,6 @@ import getpass
 # Linux
 if platform.system() == "Linux":
     source_hotkeys = Path("/home/burny/syncthing/secrets/repos/SC2Hotkeys/Burny.SC2Hotkeys")
-    # source_hotkeys2 = "/home/burny/syncthing/secrets/repos/SC2Hotkeys/BurnySteal.SC2Hotkeys"
     source_hotkeys2 = Path(
         "/home/burny/syncthing/secrets/repos/SC2Hotkeys/BurnyStealColemak.SC2Hotkeys"
     )
@@ -22,15 +21,15 @@ if platform.system() == "Linux":
     user_name = getpass.getuser()
     # normal_folder = f"/home/{user_name}/Games/battlenet/drive_c/users/{user_name}/My Documents/StarCraft II/Hotkeys"
     # accounts_folder = f"/home/{user_name}/Games/battlenet/drive_c/users/{user_name}/My Documents/StarCraft II/Accounts"
-    normal_folder = Path("/media/ssd480/Games/starcraft5/drive_c/users/burny/Documents/StarCraft II/Hotkeys/")
-    accounts_folder = Path("/media/ssd480/Games/starcraft5/drive_c/users/burny/Documents/StarCraft II/Accounts/")
+    normal_folder = Path("/media/ssd250/Games/starcraft5/drive_c/users/burny/Documents/StarCraft II/Hotkeys/")
+    accounts_folder = Path("/media/ssd250/Games/starcraft5/drive_c/users/burny/Documents/StarCraft II/Accounts/")
 # Windows
 else:
     user_name = os.environ["USERNAME"]
     normal_folder = f"C:/Users/{user_name}/Documents/StarCraft II/Hotkeys"
     accounts_folder = Path(f"C:/Users/{user_name}/Documents/StarCraft II/Accounts")
 
-if os.path.isfile(source_hotkeys):
+if source_hotkeys.is_file():
     # Copy hotkeys to all account folders
     for folder in accounts_folder.iterdir():
         target_folder = folder / "Hotkeys"
